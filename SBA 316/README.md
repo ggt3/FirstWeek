86e823703e0737ccf4fb649c39c33235a1dceafc
Georgia <9707976+ggt3@users.noreply.github.com>
2024-08-22 01:33:08 -0400

    fixed some form validation
    updated some learnings in this segment in the README

--- SBA 316/README.md
+++ SBA 316/README.md
@@ -1,1 +1,10 @@
-### For my page, I decided to make a one page journal entry where you can see the entries "saved" on the page in reverse submission order.
+### about
+For my page, I decided to make a one page journal entry where you can see the entries "saved" on the page in reverse submission order.
+
+######
+A few things I learned: 
+- the "required" keyword for an input only works as intended when all the inputs are nested in a form tag
+
+- adding an eventListener for a "submit" action only works on the form object, not the button itself
+
+- e.stopDefault() on a form submit prevented the page from refreshing, allowing me to populate my journal entries
--- SBA 316/journal.js
+++ SBA 316/journal.js
@@ -1,71 +1,66 @@
-// const app = document.body.appendChild(document.createElement("div"))
+/**
+ * NOTES
+ */
 
-// app.appendChild(document.createElement("ul"))
+const log = document.getElementById("logEntries");
+const form = document.getElementById("form");
 
+form.addEventListener("submit", addEntryToJournal);
 
-const log = document.getElementById("logEntries")
-const submitBtn = document.getElementById("submit")
-console.log(submitBtn)
-
-const form = document.getElementById("form")
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    if (input.value.trim() === '') {
-      alert('Please fill out the input field!');
-    } else {
-      // Form submission logic goes here
-      form.submit();
-    }
-  });
-submitBtn.addEventListener("click", addEntryToJournal)
 let entryCounter = 1;
 
-function addEntryToJournal() {
-    const newSect = document.createElement("div")
-    newSect.setAttribute("id",entryCounter)
-    newSect.classList.add("oneEntry") //for fomatting an entry css
-    const listAll = document.getElementById("logEntries")
-    listAll.insertBefore(newSect, listAll.firstChild) //adding the newest entry at the top
-    
-    //adding title
-    const title = document.getElementById("title")
-    const loggedTitle = document.createElement("h2")
-    loggedTitle.textContent =`${title.value}`
-    newSect.appendChild(loggedTitle)
-    title.value =""
-    
-    //adding date
-    const date = document.getElementById("entryDate")
-    const loggedDate= document.createElement("p")
-    loggedDate.style.color = "var(--date-color)";
-    loggedDate.textContent = date.value;
-    newSect.appendChild(loggedDate)
+function addEntryToJournal(e) {
+  e.preventDefault();
+  const title = document.getElementById("title");
+  const textarea = document.getElementById("journalEntry");
+  //not adding null title/entires
+  if (title.value == "" || textarea.value == "") {
+    console.log("type something");
+    return;
+  }
 
-    //adding journal entry
-    const textarea =  document.getElementById("journalEntry")
-    const loggedEntry = document.createElement("p")
-    loggedEntry.textContent = textarea.value;
-    newSect.appendChild(loggedEntry)
-    textarea.value = ""
+  const newSect = document.createElement("div");
+  newSect.style.backgroundColor = "#eaeaea";
+  newSect.setAttribute("id", entryCounter);
+  newSect.classList.add("oneEntry"); //for fomatting an entry css
+  const listAll = document.getElementById("logEntries");
+  listAll.insertBefore(newSect, listAll.firstChild); //adding the newest entry at the top
+  //add header break
+  newSect.appendChild(document.createElement("hr"));
+  //adding title
+  const loggedTitle = document.createElement("h2");
+  loggedTitle.textContent = `${title.value}`;
+  newSect.appendChild(loggedTitle);
+  title.value = "";
 
-    //add delete button for entry
-    const deleter = document.createElement("button")
-    deleter.textContent="delete entry"
-    deleter.classList.add("delete")
-    deleter.addEventListener("click", deleteEntryFromJournal)
-    newSect.appendChild(deleter)
-    //add header break
-    newSect.appendChild(document.createElement("hr"))
-    entryCounter++;
+  //adding date
+  const date = document.getElementById("entryDate");
+  const loggedDate = document.createElement("p");
+  loggedDate.style.color = "var(--date-color)";
+  loggedDate.textContent = date.value;
+  newSect.appendChild(loggedDate);
 
-    //show user successful log
-    console.log("added entry")
+  //adding journal entry
+  const loggedEntry = document.createElement("p");
+  loggedEntry.textContent = textarea.value;
+  newSect.appendChild(loggedEntry);
+  textarea.value = "";
+
+  //add delete button for entry
+  const deleter = document.createElement("button");
+  deleter.textContent = "delete entry";
+  deleter.classList.add("delete");
+  deleter.addEventListener("click", deleteEntryFromJournal);
+  newSect.appendChild(deleter);
+
+  entryCounter++;
+
+  //show user successful log
+  console.log("added entry");
 }
 
-
 function deleteEntryFromJournal(e) {
-    e.stopPropagation()
-    log.removeChild(e.target.parentNode)
+  e.stopPropagation();
+  log.removeChild(e.target.parentNode);
 }
 // const title = document.getElementById("title").required = true
